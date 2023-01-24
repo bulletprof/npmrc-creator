8f2ad29a5db292a0753fc74ef502b97878f0cd45
ds <dan@nrich.tech>
2023-01-24 16:48:40 +0200

    Fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,6 @@ if [[ -z "$INPUT_ORG_NAME" ]]; then
     echo "@nrichsolutions:registry=https://npm.pkg.github.com/" > .npmrc
 else 
     echo "@$INPUT_ORG_NAME:registry=https://npm.pkg.github.com/" > .npmrc
+fi
 
 echo "//npm.pkg.github.com/:_authToken=$INPUT_AUTH_TOKEN" >> .npmrc
