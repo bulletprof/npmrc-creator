e36742f690ed235bea75a60c7f61dc129dcc98b9
ds <dan@nrich.tech>
2023-01-30 11:23:00 +0200

    WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,10 +8,10 @@ fi
 
 echo "//npm.pkg.github.com/:_authToken=$INPUT_AUTH_TOKEN" >> .npmrc
 
-if [[ -z "$INPUT_ENV" ]] && [[ "$INPUT_ENV" = "clouz" ]]; then
-    echo "here" >> $GITHUB_OUTPUT
-    echo "$(ls)" >> $GITHUB_OUTPUT
-    echo "$(PWD)" >> $GITHUB_OUTPUT
+if [[ "$INPUT_ENV" == "clouz" ]]; then
+    echo "here" 
+    echo "$(ls)"
+    echo "$(PWD)"
     mv .npmrc ./microservices/
     echo "$(ls microservices)" >> $GITHUB_OUTPUT
 fi
